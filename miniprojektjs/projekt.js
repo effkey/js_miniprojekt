6c7f788f79de4c22cdf9ab42b475ca580d9fbcb8
Jakub Pietyszuk <94193901+effkey@users.noreply.github.com>
2022-12-04 23:45:14 +0100

    kuvba

--- miniprojektjs/projekt.js
+++ miniprojektjs/projekt.js
@@ -10,7 +10,6 @@ class Paragon {
     this.ilosc = ilosc;
     this.cena = cena;
   }
-
   addToParagon(raz, numberofClick) {
     var table = document.getElementById("btab");
     var row = table.insertRow();
@@ -43,68 +42,40 @@ class Paragon {
   }
 }
 
-let liczba = document.getElementById("liczba");
 let tablicaObiektow = [];
 function submitButton() {
-  // var table = document.getElementById('btab');
-
   let tabTotal = [];
-  let rowQuantity = 0;
+  let numberofClick = 0;
   let razem = 0;
-  let notFull = false;  // zmienna potrzebna do prawidłowego sprawdzania warunków (patrz linijka 60 i 63)
 
   document.forms.myform.onsubmit = function (event) {
-    for (const element of document.getElementsByTagName('input')) {
-      if(element.value == "") {
-        notFull = true;
-        alert("Nie podano wszystkich wymaganych wartości!")
-        break
-      }
+    /// local storage
+    numberofClick += 1;
+
+    // tworzenie nowego obiektu
+    let paragon = new Paragon(
+      document.getElementById("lname").value,
+      document.getElementById("quantity").value,
+      document.getElementById("price").value
+    );
+    tablicaObiektow.push(paragon);
+
+    // dodawanie sumy ceny elementow do tabeli razem
+    let sumResult = paragon.countSum();
+    razem = 0;
+    tabTotal.push(sumResult);
+
+    for (let i = 0; i < tabTotal.length; i++) {
+      razem += tabTotal[i];
     }
-    if(notFull) {
-      notFull = false;  // taki zabieg aby ominąć wstawienie pustego wiersza do tabeli
-    }
-    else if(isNaN(document.getElementById("price").value) & isNaN(document.getElementById("quantity").value)) {
-      alert("Podana cena i ilość nie są liczbami!")
-    }
-    else if (isNaN(document.getElementById("price").value)) {
-      alert("Podana cena nie jest liczbą!")
-    }
-    else if (isNaN(document.getElementById("quantity").value)) {
-      alert("Podana ilość nie jest liczbą!")
-    }
 
-    else {
-      /// local storage
-      rowQuantity += 1;   // ilość wierszy (indeks)
-      localStorage.setItem("nazwa", document.getElementById("lname").value);
-      localStorage.setItem("ilosc", document.getElementById("quantity").value);
-      localStorage.setItem("cena", document.getElementById("price").value);
+    paragon.addToParagon(razem, numberofClick);
+    // console.log(paragon);
+    // console.log(tabTotal);
+    // console.log(tablicaObiektow);
+    localStorage.setItem("tablicaa", JSON.stringify(tablicaObiektow));
 
-      let name = localStorage.getItem("nazwa");
-      let quantity = localStorage.getItem("ilosc");
-      let price = localStorage.getItem("cena");
-
-      // tworzenie nowego obiektu
-      let paragon = new Paragon(name, quantity, price);
-      tablicaObiektow.push(paragon);
-
-      // dodawanie sumy ceny elementow do tabeli razem
-      let sumResult = paragon.countSum();
-      razem = 0;
-      tabTotal.push(sumResult);
-
-      for (let i = 0; i < tabTotal.length; i++) {
-        razem += tabTotal[i];
-      }
-
-      paragon.addToParagon(razem, rowQuantity);
-      // console.log(paragon);
-      console.log(tabTotal);
-      console.log(tablicaObiektow);
-
-      event.preventDefault();
-    } 
+    event.preventDefault();
   };
 
   var buttonUsun = document.getElementById("usun");
@@ -127,4 +98,30 @@ function submitButton() {
     document.getElementById("razem").innerHTML = razem.toFixed(2) + " zł";
   });
 }
+
+function dodajdopar() {
+  let tab = JSON.parse(localStorage.getItem("tablicaa") || "[]");
+  console.log(tab);
+  for (let i = 0; i < tab.length; i++) {
+    var table = document.getElementById("btab");
+    var row = table.insertRow();
+    var cellId = row.insertCell();
+    cellId.innerHTML = i;
+    var cellName = row.insertCell();
+    cellName.innerHTML = tab[i].nazwa;
+    var cellQuantity = row.insertCell();
+    cellQuantity.innerHTML = tab[i].ilosc;
+    var cellPrice = row.insertCell();
+    cellPrice.innerHTML = tab[i].cena + " zł";
+    cellName.setAttribute("contentEditable", "true");
+    cellQuantity.setAttribute("contentEditable", "true");
+    cellPrice.setAttribute("contentEditable", "true");
+    let suma = tab[i].ilosc * tab[i].cena;
+    var cellSum = row.insertCell();
+    cellSum.innerHTML = suma.toFixed(2) + " zł";
+    // this.razem = raz;
+  }
+}
 submitButton();
+dodajdopar();
+// localStorage.clear();
